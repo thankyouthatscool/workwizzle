6ee35b4600cb5cde59666c79b124c8877ace0e53
thankyouthatscool <85291297+thankyouthatscool@users.noreply.github.com>
2023-08-03 13:12:00 +1000

    yes yes yes

--- apps/work-tracker/components/AppRoot/AppRoot.tsx
+++ apps/work-tracker/components/AppRoot/AppRoot.tsx
@@ -1,13 +1,36 @@
+import { useCallback, useEffect } from "react";
 import { DefaultTheme, NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
+import { useAppSelector } from "@hooks";
 import { HomeScreen } from "@screens/HomeScreen";
 import { TouchedDateScreen } from "@screens/TouchedDateScreen";
 import type { HomeScreenStackProps } from "@types";
+import { createDefaultTableSQLString } from "@utils";
 
 const HomeScreenStack = createNativeStackNavigator<HomeScreenStackProps>();
 
 export const AppRoot = () => {
+  const { databaseInstance: db } = useAppSelector(({ app }) => app);
+
+  const handleInitialLoad = useCallback(() => {
+    db.transaction(
+      (tx) => {
+        tx.executeSql(createDefaultTableSQLString, []);
+      },
+      (err) => {
+        console.log(err);
+      },
+      () => {
+        console.log("Database OK");
+      }
+    );
+  }, []);
+
+  useEffect(() => {
+    handleInitialLoad();
+  }, []);
+
   return (
     <NavigationContainer
       theme={{
--- apps/work-tracker/screens/TouchedDateScreen/TouchedDateScreen.tsx
+++ apps/work-tracker/screens/TouchedDateScreen/TouchedDateScreen.tsx
@@ -1,5 +1,5 @@
 import { useNavigation } from "@react-navigation/native";
-import { useState, type FC } from "react";
+import { type FC, useEffect, useState } from "react";
 import { View } from "react-native";
 import { IconButton, Text, TextInput } from "react-native-paper";
 import { SafeAreaView } from "react-native-safe-area-context";
@@ -7,23 +7,51 @@ import { SafeAreaView } from "react-native-safe-area-context";
 import { useAppDispatch, useAppSelector } from "@hooks";
 import { setTouchedDateInformation } from "@store";
 import { DEFAULT_APP_PADDING } from "@theme";
-import { getMonthInformation } from "@utils";
+import { DbMonthData } from "@types";
 
 export const TouchedDateScreen = () => {
   const dispatch = useAppDispatch();
 
-  const { databaseInstance: db, touchedDateInformation } = useAppSelector(
-    ({ app }) => app
-  );
+  const {
+    appSettings: {
+      appDefaults: {
+        DEFAULT_COMMENT,
+        DEFAULT_DAILY_HOURS,
+        DEFAULT_HOURLY_RATE,
+        DEFAULT_WEEKLY_HOURS,
+      },
+    },
+    databaseInstance: db,
+    touchedDateInformation,
+  } = useAppSelector(({ app }) => app);
 
-  const [touchedMonthInformation] = useState<
-    ReturnType<typeof getMonthInformation>
-  >(() =>
-    getMonthInformation(
-      touchedDateInformation?.TOUCHED_YEAR!,
-      touchedDateInformation?.TOUCHED_MONTH!
-    )
-  );
+  const [formData, setFormData] = useState<{
+    comment: string;
+    hourlyRate: string;
+    hoursWorked: string[];
+  }>({
+    comment: DEFAULT_COMMENT,
+    hourlyRate: DEFAULT_HOURLY_RATE,
+    hoursWorked: [DEFAULT_DAILY_HOURS],
+  });
+
+  useEffect(() => {
+    db.transaction(
+      (tx) => {
+        tx.executeSql(
+          "SELECT * FROM dayTracker",
+          [],
+          (_, { rows: { _array } }) => {
+            console.log(_array);
+          }
+        );
+      },
+      (err) => {
+        console.log(err);
+      },
+      () => {}
+    );
+  }, []);
 
   return (
     <SafeAreaView>
@@ -32,20 +60,56 @@ export const TouchedDateScreen = () => {
           dispatch(setTouchedDateInformation(null));
         }}
       />
-      {!!touchedDateInformation && (
-        <View>
-          <Text>{touchedDateInformation.TOUCHED_DATE}</Text>
-          <Text>{touchedDateInformation.TOUCHED_MONTH}</Text>
-          <Text>{touchedDateInformation.TOUCHED_YEAR}</Text>
-          <Text>{touchedMonthInformation.monthNameLong}</Text>
-        </View>
-      )}
+      <View style={{ flexDirection: "row" }}>
+        <TextInput
+          contextMenuHidden
+          keyboardType="numeric"
+          label="Hours Worked"
+          mode="outlined"
+          onChangeText={(newHoursWorked) => {
+            setFormData((formData) => ({
+              ...formData,
+              hoursWorked: [newHoursWorked],
+            }));
+          }}
+          right={
+            <TextInput.Affix
+              text={
+                parseFloat(formData.hoursWorked[0]) === 1 ? "hour" : "hours"
+              }
+            />
+          }
+          style={{
+            flex: 1,
+            marginLeft: DEFAULT_APP_PADDING,
+            marginRight: DEFAULT_APP_PADDING / 2,
+          }}
+          value={formData.hoursWorked[0]}
+        />
+        <TextInput
+          contextMenuHidden
+          keyboardType="numeric"
+          label="Hourly Rate"
+          mode="outlined"
+          left={<TextInput.Affix text="$" />}
+          style={{
+            flex: 1,
+            marginLeft: DEFAULT_APP_PADDING / 2,
+            marginRight: DEFAULT_APP_PADDING,
+          }}
+          value={formData.hourlyRate}
+        />
+      </View>
       <TextInput
         label="Comment"
         mode="outlined"
         multiline
         numberOfLines={4}
+        onChangeText={(newDayComment) => {
+          setFormData((formData) => ({ ...formData, comment: newDayComment }));
+        }}
         style={{ marginHorizontal: DEFAULT_APP_PADDING }}
+        value={formData.comment}
       />
     </SafeAreaView>
   );
